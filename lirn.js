ce19a1de459a0e5b50d90a57c4bdcc46ad691a80
Arthur Clayton <arthurclayton@karin-win7.ryokan.local>
2018-08-20 19:28:42 -0700

    help with getting correct data for my var

--- lirn.js
+++ lirn.js
@@ -16,7 +16,7 @@ var client = new Twitter(keys.twitterKeys);
 var params = {
     screen_name: 'Arthur Clayton',
     count: 21,
-}
+};
 client.get('statuses/user_timeline', params, function(error, tweets, response){
     if (!error) {
         for(var i = 1; i <= 20; i++){
@@ -29,24 +29,55 @@ client.get('statuses/user_timeline', params, function(error, tweets, response){
           console.log(error);
       }
     });   
-}
+};
 
 function spotifyData(){
+    if(process.argv.length >=4 || typeof uSelection === 'string'){
+    var spotify = new Spotify(keys.spotifyKeys);
+spotify.search({ type: 'track', query: uSelection }, function(err, data) {
+    if (!err) {
+        spotifyInfo(data);
+    }
+    else {
+        console.log('Error ' + err);
+        return;
+      }
+    
+  });
+}
+};
+
+function spotifyInfo(data){
+    var artist = 
+    var album = 
+    var song = 
+    var songLink = 
+    console.log('Artist ' + artist);
+    console.log('Album ' + album);
+    console.log('Track ' + song);
+    console.log('Link to song ' + songLink);
+    
+};
+
+switch(uInput){
+case 'my-tweets':
+twitdata();
+break;
+
+case 'spotify-this-song':
+spotifyData();
+break;
+}
+// test function to grab data to be used in the function spotifyInfo
+function t(){
     var spotify = new Spotify(keys.spotifyKeys);
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
    
-    console.log(JSON.stringify(data, null, 2));
+    console.log(JSON.stringify(data, null, 2)); 
   });
-}
-spotifyData();
+};
 
-switch(uInput){
-case `my-tweets`:
-twitdata();
-break;
-}
-
-// Do something with 'data'
+t();
