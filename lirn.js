661e4a9abd347b4db14d69a2834bebf8f2947bad
Arthur Clayton <arthurclayton@karin-win7.ryokan.local>
2018-08-21 23:06:57 -0700

    have ombd function working

--- lirn.js
+++ lirn.js
@@ -9,7 +9,8 @@ var keys = require('./keys');
 var uInput = process.argv[2];
 var uSelection = process.argv[3];
 
-var queryUrl = 'http://www.omdbapi.com/?apikey=c7ea6710&t=' + uSelection + '&y=&plot=short&r=json'
+
+var queryUrl = 'http://www.omdbapi.com/?apikey=c7ea6710&t=' + uSelection + '&y=&plot=short&tomatoes=true&r=json';
 
 //twitter function data
 function twitdata(){
@@ -33,7 +34,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response){
 };
 //spotify functions
 function spotifyData(){
-    if(process.argv.length >=4 || typeof uSelection === 'string'){
+    if(process.argv.length >= 4 || typeof uSelection === 'string'){
     var spotify = new Spotify(keys.spotifyKeys);
 spotify.search({ type: 'track', query: uSelection }, function(err, data) {
     if (!err) {
@@ -76,21 +77,27 @@ function spotifyInfo(data){
 // ombd functions
 
 function ombdData(){
-if(process.argv.length >=4 || typeof uSelection === 'string'){
-request(queryUrl, function(error, response, body) {
-
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-  
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log("Year: " + JSON.parse(body).year);
-    }
-    else{
-        console.log('error:', error);
-    }
-  });
-}};
+    if(process.argv.length >=4 || typeof uSelection === 'string'){
+    request(queryUrl, function(error, response, body) {
+    
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+        // Parse the body of the site 
+          console.log("Title: " + JSON.parse(body).Title);
+          console.log("Year: " + JSON.parse(body).Year);
+          console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
+          console.log("Country: " + JSON.parse(body).Country);
+          console.log("Lanuage: " + JSON.parse(body).Language);
+          console.log("Plot: " + JSON.parse(body).Plot);
+          console.log("Actors: " + JSON.parse(body).Actors);
+          console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+          console.log("Rotten Tomatoes Url: " + JSON.parse(body).tomatoURL);
+        }
+        else{
+            console.log('error:', error);
+        }
+      });
+    }};
 
 
 
