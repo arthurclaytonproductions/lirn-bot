e4cb52ed05977b78a7ef678dc6bdc95160a4c84c
Arthur Clayton <arthurclayton@Arthurs-MacBook-Pro.local>
2018-08-22 22:43:01 -0700

    finished product

--- lirn.js
+++ lirn.js
@@ -5,7 +5,7 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var keys = require('./keys');
-
+var fs = require('fs');
 var uInput = process.argv[2];
 var uSelection = process.argv[3];
 
@@ -83,23 +83,62 @@ function ombdData(){
         // If the request is successful
         if (!error && response.statusCode === 200) {
         // Parse the body of the site 
-          console.log("Title: " + JSON.parse(body).Title);
-          console.log("Year: " + JSON.parse(body).Year);
-          console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
-          console.log("Country: " + JSON.parse(body).Country);
-          console.log("Lanuage: " + JSON.parse(body).Language);
-          console.log("Plot: " + JSON.parse(body).Plot);
-          console.log("Actors: " + JSON.parse(body).Actors);
-          console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-          console.log("Rotten Tomatoes Url: " + JSON.parse(body).tomatoURL);
+            var jpBody = JSON.parse(body);
+            ombdInfo(jpBody);
         }
         else{
             console.log('error:', error);
         }
       });
+    }
+      else if(process.argv.length < 4){
+        request('http://www.omdbapi.com/?apikey=c7ea6710&t=Mr.+Nobody&y=&plot=short&tomatoes=true&r=json', function(error, response, body) {
+        
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+            // Parse the body of the site 
+                var jpBody = JSON.parse(body);
+                ombdInfo(jpBody);
+                  
+    }
+            else{
+            console.log('error:', error);
+            }
+        });
     }};
 
+function ombdInfo(body){
+          console.log("Title: " + body.Title);
+          console.log("Year: " + body.Year);
+          console.log("IMBD Rating: " + body.imbdRating);
+          console.log("Country: " + body.Country);
+          console.log("Lanuage: " + body.Language);
+          console.log("Plot: " + body.Plot);
+          console.log("Actors: " + body.Actors);
+          console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+          console.log("Rotten Tomatoes Url: " + body.tomatoURL);
+};
 
+function randomText(){
+    fs.readFile('random.txt', 'utf8', function(err, data){
+        if (!err) {
+           var dataArr = data.split(",");
+           uInput = dataArr[0];
+           uSelection = dataArr[1];
+           
+           switch(uInput){
+               case 'spotify-this-song':
+               spotifyData();
+               break;
+           }
+           
+              
+        }
+        else{
+            return console.log('Error occurred: ' + err);
+        }
+      });
+}
 
 switch(uInput){
 case 'my-tweets':
@@ -112,6 +151,10 @@ break;
 
 case 'movie-this':
 ombdData();
+break;
+
+case 'do-what-it-says':
+randomText();
 break;
 }
 
